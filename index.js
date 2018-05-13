12f289795082c33bc5e15f39af9711088413c032
James Didehvar <james@didehvar.com>
2018-05-13 12:58:07 +0100

    allow fetching of bitfinex symbols

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var Koa = require('koa');
 var Router = require('koa-router');
+const fetch = require('node-fetch');
 
 var app = new Koa();
 var router = new Router();
 
 router.get('/', (ctx, next) => {
   ctx.body = 'hello world';
+});
+
+router.get('/symbols', async ctx => {
+  const res = await fetch('https://api.bitfinex.com/v1/symbols');
+  ctx.body = await res.json();
 });
 
 app.use(router.routes()).use(router.allowedMethods());
